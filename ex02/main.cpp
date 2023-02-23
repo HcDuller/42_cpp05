42615e1a99e979ca5e7aad3b1284d392657b36a7
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-02-23 19:04:59 -0300

    removed comments from ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 18:26:58 by hde-camp          #+#    #+#             */
-/*   Updated: 2023/02/23 17:17:17 by hde-camp         ###   ########.fr       */
+/*   Updated: 2023/02/23 19:04:47 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,48 +54,6 @@ void bureaucratTest(){
 	std::cout << "Heap allocation end" << std::endl << std::endl;
 	std::cout << std::endl << "💼💼💼💼BUREAUCRAT END💼💼💼💼" << std::endl;
 };
-// void formTest(){
-// 	std::cout << std::endl << "📜📜📜📜FORM START📜📜📜📜" << std::endl;
-// 	std::cout << "Stack allocation start" << std::endl;
-// 	{
-// 		Bureaucrat hermes("Hermes",33);
-// 		Form	laundry("Laundy", 50, 140);
-// 		Form	acc("Accounting", 30, 50);
-// 		std::cout << hermes << std::endl;
-// 		std::cout << std::endl;
-// 		std::cout << laundry << std::endl;
-// 		std::cout << std::endl;
-// 		std::cout << acc << std::endl;
-// 		std::cout << "Hermes should be able to sign Laundry, but not Accounting." << std::endl;
-// 		hermes.signForm(laundry);
-// 		hermes.signForm(acc);
-// 		std::cout << std::endl;
-// 		std::cout << laundry << std::endl;
-// 		std::cout << std::endl;
-// 		std::cout << acc << std::endl;
-// 	}
-// 	std::cout << "Stack allocation end" << std::endl << std::endl;
-// 	std::cout << "Heap allocation start" << std::endl;
-// 	{
-// 		Bureaucrat* hermes = new Bureaucrat("Hermes",33);
-// 		Form*	laundry = new Form("Laundy", 50, 140);
-// 		Form*	acc = new Form("Accounting", 30, 50);
-// 		std::cout << *hermes << std::endl;
-// 		std::cout << std::endl;
-// 		std::cout << *laundry << std::endl;
-// 		std::cout << std::endl;
-// 		std::cout << *acc << std::endl;
-// 		std::cout << "Hermes should be able to sign Laundry, but not Accounting." << std::endl;
-// 		hermes->signForm(*laundry);
-// 		hermes->signForm(*acc);
-// 		delete hermes;
-// 		delete laundry;
-// 		delete acc;
-// 	}
-// 	std::cout << "Heap allocation end" << std::endl << std::endl;
-// 	std::cout << std::endl << "📜📜📜📜FORM END📜📜📜📜" << std::endl;
-
-// };
 void shrubberyTest(){
 	std::cout << std::endl << "🌳🌳🌳🌳SHRUBBERY START🌳🌳🌳🌳" << std::endl << std::endl;
 	{
